50e3641b4413f9ff85a4877ef4afd8b1d9ee95c0
ringoyu2551997tn <antao6125@gmail.com>
2024-08-24 12:45:54 +0700

    New Commit

--- book-network/src/main/java/com/alibou/book/security/UserDetailsServiceImpl.java
+++ book-network/src/main/java/com/alibou/book/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alibou.book.security;
 
 import com.alibou.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository repository;
